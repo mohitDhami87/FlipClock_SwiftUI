86efe83d15f647174f6de85848fe12e464776ac4
Mohit Sharma <mohit@techies.it>
2025-01-08 16:50:20 +0530

    Initial Commit

---
+++ FlipClock_SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlipClock_SwiftUI
+//
+//  Created by Mohit Sharma on 08/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlipClock_SwiftUI/FlipClock_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlipClock_SwiftUIApp.swift
+//  FlipClock_SwiftUI
+//
+//  Created by Mohit Sharma on 08/01/25.
+//
+
+import SwiftUI
+
+@main
+struct FlipClock_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
